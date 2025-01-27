26a0741da2d07e8f34ecc6c9be8f6e280e077e1f
Wanderson37 <wanderson_c.o@hotmail.com>
2025-01-27 09:17:51 -0400

    install quasar

--- impetusfit/src/main.js
+++ impetusfit/src/main.js
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
+import { Quasar } from "quasar";
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/src/css/index.sass";
 
-createApp(App).use(createPinia()).mount("#app");
+const myApp = createApp(App).use(createPinia()).mount("#app");
+
+myApp.use(Quasar, {
+  plugins: {},
+});
+
+myApp.mount("#app");
